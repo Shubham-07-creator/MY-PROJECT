c4016f7f8a2e74203a03500e593054fb5129a483
SHUBHAM KUMAR <shubhamkumar979883@gmail.com>
2026-03-02 12:10:08 +0530

    My 1st Project

--- controllers/listings.js
+++ controllers/listings.js
@@ -110,24 +110,62 @@ module.exports.editListing = async (req, res) => {
     res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
+// module.exports.updateListing = async (req, res) => {
+//         if (!req.body.listing) {
+//             throw new ExpressError(400, "send valid data for listing");
+//         }
+//     let {id} = req.params;
+//     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    
+//     if(typeof req.file !== "undefined") {
+//     let url = req.file.path;
+//     let filename = req.file.filename;
+//     listing.image = { url, filename, };
+//     await listing.save();
+//     }
+    
+//     req.flash("success", "Listing updated successfully");
+//     res.redirect(`/listings/${id}`);
+// };
+
+
 module.exports.updateListing = async (req, res) => {
-        if (!req.body.listing) {
-            throw new ExpressError(400, "send valid data for listing");
-        }
-    let {id} = req.params;
-    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    
-    if(typeof req.file !== "undefined") {
-    let url = req.file.path;
-    let filename = req.file.filename;
-    listing.image = { url, filename, };
-    await listing.save();
+
+    if (!req.body.listing) {
+        throw new ExpressError(400, "send valid data for listing");
     }
-    
+
+    let { id } = req.params;
+
+    // 🔥 LOCATION → NEW COORDINATES
+    let response = await geocodingClient
+        .forwardGeocode({
+            query: req.body.listing.location,
+            limit: 1,
+        })
+        .send();
+
+    req.body.listing.geometry =
+        response.body.features[0].geometry;
+
+    // listing update
+    let listing = await Listing.findByIdAndUpdate(
+        id,
+        { ...req.body.listing },
+        { new: true }
+    );
+
+    // image update
+    if (typeof req.file !== "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+
     req.flash("success", "Listing updated successfully");
     res.redirect(`/listings/${id}`);
 };
-
 
 module.exports.deleteListing = async (req, res) => {
     let {id} = req.params;
--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
+    style: 'mapbox://styles/mapbox/streets-v12', // Use the standard style for the map
     center: listing.geometry.coordinates, // center the map on this longitude and latitude
     zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
 
